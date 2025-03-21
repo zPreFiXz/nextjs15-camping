330c823f908bc721ce2b0483315059a362938e06
Phakphoom Sukkhachat <prefix.2pm@gmail.com>
2025-03-21 16:35:31 +0700

    Implement user authentication check and profile creation logic

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,18 @@
 "use server";
 
 import { profileSchema, validateWithZod } from "@/utils/schemas";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import db from "@/utils/db";
+import { redirect } from "next/navigation";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must logged!!!");
+  }
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
 
 const renderError = (error: unknown): { message: string } => {
   return {
@@ -13,12 +25,26 @@ export const createProfileAction = async (
   formData: FormData
 ) => {
   try {
+    const user = await getAuthUser();
     const rawData = Object.fromEntries(formData);
     const validateField = validateWithZod(profileSchema, rawData);
-    console.log(validateField);
-    return { message: "Create Profile Successfully!!!" };
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validateField,
+      },
+    });
+    const clerk = await clerkClient();
+    await clerk.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
     console.log(error);
     return renderError(error);
   }
+  redirect("/");
 };
